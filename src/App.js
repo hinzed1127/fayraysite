e6690cb3d4dc97c3310c967f580811c34bc33d01
Dan Hinze <hinzed1127@gmail.com>
2017-07-13 14:09:51 -0500

    Add initial layout and styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Rowbox from './components/Rowbox';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <main className='container'>
+        <div className='row row1'>
+          <h1>Fay Ray</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className='row'>
+          <Rowbox text='Is a band'/>
+          <Rowbox text='Has a new single'/>
+        </div>
+        <div className='row'>
+          <Rowbox text='Plays shows'/>
+          <Rowbox text='Takes photos'/>
+          <Rowbox text='Uses email'/>
+        </div>
+      </main>
     );
   }
 }
---
+++ src/components/Rowbox.js
@@ -0,0 +1,10 @@
+// @flow
+import React from 'react';
+
+export default function Rowbox(props) {
+	return (
+		<div className='rowbox'>
+			{props.text}
+		</div>
+	);
+}
