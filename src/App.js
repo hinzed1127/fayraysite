3d2e0652044104773b9065c49f32db07629ac103
Dan Hinze <hinzed1127@gmail.com>
2017-07-13 20:25:58 -0500

    Make rows responsive, add icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import Rowbox from './components/Rowbox';
+import IconRow from './components/IconRow';
 
 class App extends Component {
   render() {
@@ -9,12 +9,13 @@ class App extends Component {
       <main className='container'>
         <div className='row row1'>
           <h1>Fay Ray</h1>
+          <IconRow />
         </div>
-        <div className='row'>
+        <div className='row row2'>
           <Rowbox text='Is a band'/>
           <Rowbox text='Has a new single'/>
         </div>
-        <div className='row'>
+        <div className='row row3'>
           <Rowbox text='Plays shows'/>
           <Rowbox text='Takes photos'/>
           <Rowbox text='Uses email'/>
