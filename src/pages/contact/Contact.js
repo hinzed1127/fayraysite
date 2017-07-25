3afd27c5be8e452cc36831bf0c46598f4c64d13b
Dan Hinze <hinzed1127@gmail.com>
2017-07-25 11:54:56 -0500

    Clean up global styling, page descriptions

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 export default function Contact() {
 	return (
-		<div>
-			send us yo digits
+		<div className='contact-container'>
+			<h1>Booking/Management</h1>
+			<h2>Dan Hinze</h2>
+			<h3>dan.hinze.booking@gmail.com</h3>
+
+			<h1>Press</h1>
+			<h3>general@fayray.band</h3>
 		</div>
 	);
 }
--- src/pages/space/Space.js
+++ src/pages/space/Space.js
@@ -10,28 +10,36 @@ import Tom from '../../img/tom.png';
 
 export default function Space() {
 	return (
-		<div className='space'>
-			<h1 className='nameHeader'>FAY RAY</h1>
-			<div className='person joe'>
-				<img src={Joe} alt='Surfing Joe'/>
+		<div>
+			<div className='space'>
+				<h1 className='nameHeader'>FAY RAY</h1>
+				<div className='person joe'>
+					<img src={Joe} alt='Surfing Joe'/>
+				</div>
+				<div className='person erik'>
+					<img src={Erik} alt='Erik kitty'/>
+				</div>
+				<div className='person dan'>
+					<img src={Dan} alt='Dan' />
+				</div>
+				<div className='person rob'>
+					<img src={Rob} alt='Rob' />
+				</div>
+				<div className='person noah'>
+					<img src={Noah} alt='Noah' />
+				</div>
+				<div className='person murl'>
+					<img src={Murl} alt='Mariel' />
+				</div>
+				<div className='person tom'>
+					<img src={Tom} alt='Tom' />
+				</div>
 			</div>
-			<div className='person erik'>
-				<img src={Erik} alt='Erik kitty'/>
-			</div>
-			<div className='person dan'>
-				<img src={Dan} alt='Dan' />
-			</div>
-			<div className='person rob'>
-				<img src={Rob} alt='Rob' />
-			</div>
-			<div className='person noah'>
-				<img src={Noah} alt='Noah' />
-			</div>
-			<div className='person murl'>
-				<img src={Murl} alt='Mariel' />
-			</div>
-			<div className='person tom'>
-				<img src={Tom} alt='Tom' />
+
+			<div className='bio-container'>
+				<p>Fay Ray is a band from Chicago that plays a weird amalgamation of genres and just wants to make you dance.</p>
+
+				<p>Fay Ray is Mariel, Dan, Joe, Tom, Erik, Noah, Rob, and Byron.</p>
 			</div>
 		</div>
 	);
