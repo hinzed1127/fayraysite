20e1e9a33e1a28e4621394ec74d4bb274c8ee952
Dan Hinze <hinzed1127@gmail.com>
2017-08-09 13:18:46 -0500

    Update links

--- src/constants/Links.js
+++ src/constants/Links.js
@@ -1,8 +1,8 @@
 export const LINKS = {
 	BANDCAMP: 'https://fayray.bandcamp.com/',
-	ITUNES: '#',
+	ITUNES: 'https://itunes.apple.com/us/album/turn-the-other-cheek/id1050501692',
 	YOUTUBE:'https://www.youtube.com/channel/UCUlnQZVO4BFsRenGyseQS8w',
-	FACEBOOK: 'https://www.facebook.com/churchbooty/',
+	FACEBOOK: 'https://www.facebook.com/fayraymusic/',
 	INSTAGRAM: 'https://www.instagram.com/fayraymusic/',
 	SPOTIFY: 'https://open.spotify.com/artist/5lnuY7eImt0gUeRgLLm2UU',
 	SOUNDCLOUD: 'https://soundcloud.com/fay-ray-music'
