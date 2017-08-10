109806e039133a9bea0c4ebb0a8c82be54e5f567
Dan Hinze <hinzed1127@gmail.com>
2017-08-10 14:06:12 -0500

    Update contact info

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -6,10 +6,10 @@ export default function Contact() {
 		<div className='contact-container'>
 			<h1>Booking/Management</h1>
 			<h2>Dan Hinze</h2>
-			<h3>dan.hinze.booking@gmail.com</h3>
+			<h3>hinzed1127@gmail.com</h3>
 
-			<h1>Press</h1>
-			<h3>general@fayray.band</h3>
+			<h1>General Contact</h1>
+			<h3>fayrayband@gmail.com</h3>
 		</div>
 	);
 }
