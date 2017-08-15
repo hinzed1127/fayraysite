f6f7a3bda73315625d448558da66636c51b02bf9
Dan Hinze <hinzed1127@gmail.com>
2017-08-15 13:51:46 -0500

    Add photos to media page

--- src/pages/multimedia/Multimedia.js
+++ src/pages/multimedia/Multimedia.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import './multimedia.css'
+import './multimedia.css';
+import alley from '../../img/media/alley.jpg';
+import lincolnHall from '../../img/media/lincoln-hall.jpg';
+import milk from '../../img/media/milk.jpg';
+import pygmalion from '../../img/media/pygmalion.jpg';
+import alleyWalking from '../../img/media/alley-walking.jpg';
+
 
 export default function Multimedia() {
 	return (
@@ -10,12 +16,17 @@ export default function Multimedia() {
 						className='sofar-video'
 						title='Sofar Sounds Chicago: Fay Ray'
 						src='https://www.youtube.com/embed/SmwPLvajZFo'
-						frameborder='0'
-						allowfullscreen
+						frameBorder='0'
+						allowFullScreen
 					/>
 				</div>
 			</div>
 			<div className='photos-container'>
+				<img src={alley} alt='Alley Promo'/>
+				<img src={lincolnHall} alt='Lincoln Hall Show'/>
+				<img src={milk} alt='Milk Promo'/>
+				<img src={pygmalion} alt='Pygmalion Show'/>
+				<img src={alleyWalking} alt='Alley Walking Promo'/>
 			</div>
 		</div>
 	);
