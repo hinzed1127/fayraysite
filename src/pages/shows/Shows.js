3b1aff552d9f47ab0001fd5f6bc27c2121153125
Dan Hinze <hinzed1127@gmail.com>
2017-08-19 15:24:25 -0500

    Update next show

--- src/pages/shows/Shows.js
+++ src/pages/shows/Shows.js
@@ -28,52 +28,44 @@ function toggleGif() {
 
 
 export default function Shows() {
-	const googleMapsLink = 'https://goo.gl/maps/FfZM5KfPnxn'
-	const lhEvent = 'http://www.lh-st.com/Shows/08-19-2017+The+North+41';
-	const northFortyOneLink = 'https://www.youtube.com/watch?v=MOA69DGX-UU';
-	const steadyFlowlink = 'https://www.youtube.com/watch?v=gUm6sAXjuws';
-	const akenyaLink = 'https://www.youtube.com/watch?v=U-DvsejV21g';
-	const wowGif = 'https://media.giphy.com/media/3o8dFn0ixg0rtwlQnC/giphy.gif';
-	const fbEvent = 'https://www.facebook.com/events/105253993411938/';
-
+	const googleMapsLink = 'https://goo.gl/maps/FQRtpM4hKv92';
+	const fbEvent = 'https://www.facebook.com/events/147660745797262/?acontext=%7B%22source%22%3A5%2C%22page_id_source%22%3A565113923559325%2C%22action_history%22%3A[%7B%22surface%22%3A%22page%22%2C%22mechanism%22%3A%22main_list%22%2C%22extra_data%22%3A%22%7B%5C%22page_id%5C%22%3A565113923559325%2C%5C%22tour_id%5C%22%3Anull%7D%22%7D]%2C%22has_source%22%3Atrue%7D';
+	const bandcampLink = 'https://bandcamp.com/EmbeddedPlayer/track=1513169463/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/' 
 	return (
 		<div>
 			<h1 className='shows-header'>Next Show:</h1>
 			<div className='shows-container'>
 				<div className='show-poster'>
-					<img className='poster-img' src={lhPoster} alt='Upcoming show at Lincoln Hall, August 19th' />
+					<div 
+						className='fb-video' 
+						data-href='https://www.facebook.com/crofton.coleman/videos/1309759969123037/'
+						data-width='500' 
+						data-show-text='false'
+						data-autoplay='true'
+					/>
 				</div>
 				<div className='shows-description'>
 					<div className='shows-row'>
-						We're playing at <a href={googleMapsLink}>Lincoln Hall</a>
+						<a href={googleMapsLink}>Cowboy Monkey</a>
 					</div>
 					<div className='shows-row'>
-						Tickets are cheaper if you buy them <a href={lhEvent}>now</a>
+						Champaign-Urbana, IL
 					</div>
 					<div className='shows-row'>
-						<a href={northFortyOneLink}>The North 41</a>,
-						<a href={steadyFlowlink}>Steady Flow</a>, <span>AND </span>
-						<a href={akenyaLink}> Akenya</a>.
+						<a href={fbEvent}>RSVP</a>
 					</div>
-					<div className='shows-row gif-container'>
-						<span
-							className = 'we-know'
-							onClick={() => toggleGif()}
+					<div className='shows-row'>
+						Supporting Act:
+						<iframe
+							title='Melvin Knight Bandcamp'
+							style={{border: '0', width: '262px', height: '375px'}}
+							src={bandcampLink}
+							seamless
 						>
-							We know.
-						</span>
-						<div className = 'close-gif-icon hide' onClick={() => toggleGif()}>
-							<Icon icon={ICONS.CLOSE_CIRCLE} fill='#F00' />
-						</div>
-						<img
-							className = 'wow-gif hide'
-							onClick={() => toggleGif()}
-							src={wowGif}
-							alt='wow gif'
-						/>
-					</div>
-					<div className='shows-row'>
-						Tell those folks on <a href={fbEvent}>Facebook</a> you're going, too
+							<a href='http://croftoncoleman.bandcamp.com/track/kingdom-of-heaven-weekend-warriors'>
+								Kingdom of Heaven/ Weekend Warriors by Melvin Knight
+							</a>
+						</iframe>
 					</div>
 				</div>
 			</div>
