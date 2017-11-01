399c69106072781f549148a9abfa88764403b8e8
Dan Hinze <hinzed1127@gmail.com>
2017-11-01 13:40:07 -0500

    Update soundcloud link

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import './Home.css';
 import Rowbox from '../../components/Rowbox/Rowbox';
 import IconRow from '../../components/IconRow/IconRow';
-
 class Home extends Component {
   render() {
-    const soundcloudLink = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/362959626&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true';
+    const soundcloudLink = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/364242659&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true';
     return (
       <main className='container'>
         <div className='row row1'>
