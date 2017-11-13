17b4bb0377229e5d1e4818d108a5ced06f6b9d43
Dan Hinze <hinzed1127@gmail.com>
2017-11-13 15:33:40 -0600

    Update bio, hover state for faces

--- src/pages/space/Space.js
+++ src/pages/space/Space.js
@@ -13,34 +13,91 @@ export default function Space() {
 		<div>
 			<div className='space'>
 				<h1 className='nameHeader'>FAY RAY</h1>
-				<div className='person joe'>
-					<img src={Joe} alt='Surfing Joe'/>
+				<div className='person'>
+					<div className='joe'>
+						<img src={Joe} alt='Surfing Joe'/>
+					</div>
+					<div className='description'>
+						<div className='description-text'>
+							<div>Joe</div>
+							<div>Keys/Synths</div>
+						</div>
+					</div>
 				</div>
-				<div className='person erik'>
-					<img src={Erik} alt='Erik kitty'/>
+				<div className='person'>
+					<div className='erik'>
+						<img src={Erik} alt='Erik kitty'/>
+					</div>
+					<div className='description'>
+						<div className='description-text'>
+							<div>Erik</div>
+							<div>Bass</div>
+						</div>
+					</div>
 				</div>
-				<div className='person dan'>
-					<img src={Dan} alt='Dan' />
+				<div className='person'>
+					<div className='dan'>
+						<img src={Dan} alt='Dan' />
+					</div>
+					<div className='description'>
+						<div className='description-text'>
+							<div>Dan</div>
+							<div>Saxes</div>
+						</div>
+					</div>
 				</div>
-				<div className='person rob'>
-					<img src={Rob} alt='Rob' />
+				<div className='person'>
+					<div className='rob'>
+						<img src={Rob} alt='Rob' />
+					</div>
+					<div className='description'>
+						<div className='description-text'>
+							<div>Rob</div>
+							<div>Keys</div>
+						</div>
+					</div>
 				</div>
-				<div className='person noah'>
-					<img src={Noah} alt='Noah' />
+				<div className='person'>
+					<div className='noah'>
+						<img src={Noah} alt='Noah' />
+					</div>
+					<div className='description'>
+						<div className='description-text'>
+							<div>Noah</div>
+							<div>Guitar</div>
+						</div>
+					</div>
 				</div>
-				<div className='person murl'>
-					<img src={Murl} alt='Mariel' />
+				<div className='person'>
+					<div className='murl'>
+						<img src={Murl} alt='Mariel' />
+					</div>
+					<div className='description'>
+						<div className='description-text'>
+							<div>Mariel</div>
+							<div>Vocals</div>
+						</div>
+					</div>
 				</div>
-				<div className='person tom'>
-					<img src={Tom} alt='Tom' />
+				<div className='person'>
+					<div className='tom'>
+						<img src={Tom} alt='Tom' />
+					</div>
+					<div className='description'>
+						<div className='description-text'>
+							<div>Tom</div>
+							<div>Drums</div>
+						</div>
+					</div>
 				</div>
 			</div>
 
 			<div className='bio-container'>
 				<p className='bio'>
-					Fay Ray (FKA Church Booty) is a band from Chicago that plays a weird amalgamation of genres and just wants to make you dance.
-					With high-energy performances that highlight their musical versatility and strong chemistry,
-					the group balances songs that have all the catchiness of pop, all the grooves of funk and R&B, and harmonies and improvisations equally at home in jazz.
+					Fay Ray (formerly Church Booty) has evolved. With 8 moving parts topped by the electric vocals of Mariel Fechik, 
+					the Chicago band's funk and jazz influence grows into a character all its own. Fay Ray's debut EP “No Love,” released Oct 2017, 
+					brings the band's chemistry and collaboration into the spotlight, with 4 diverse tracks that explore the intricacy of romantic relationships. 
+					Heading out of a packed 2017, Fay Ray confidently steps into their new identity. Catch them in full force at Emporium on 11/27, just a taste of what’s to come.
 				</p>
 
 				<p className='members'>Fay Ray is Mariel, Dan, Joe, Tom, Erik, Noah, Rob, and Byron.</p>
