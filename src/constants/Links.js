d10defbed030f93200fbff660f796d4170f19d8f
Dan Hinze <hinzed1127@gmail.com>
2018-07-30 17:04:46 -0500

    Update Youtube link

--- src/constants/Links.js
+++ src/constants/Links.js
@@ -1,7 +1,7 @@
 export const LINKS = {
 	BANDCAMP: 'https://fayray.bandcamp.com/',
 	ITUNES: 'https://itunes.apple.com/us/album/turn-the-other-cheek/id1050501692',
-	YOUTUBE:'https://www.youtube.com/channel/UCUlnQZVO4BFsRenGyseQS8w',
+	YOUTUBE:'https://www.youtube.com/watch?v=SmwPLvajZFo',
 	FACEBOOK: 'https://www.facebook.com/fayraymusic/',
 	INSTAGRAM: 'https://www.instagram.com/fayraymusic/',
 	SPOTIFY: 'https://open.spotify.com/artist/5lnuY7eImt0gUeRgLLm2UU',
